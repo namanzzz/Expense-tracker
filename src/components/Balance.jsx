473caa1d0b34655e1b2137f4390a11e6dfd44111
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-11-23 00:55:53 +0530

    Add files via upload

---
+++ src/components/Balance.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export const Balance = () => {
+  return (
+    <>
+            <h4>Your Balance</h4>
+            <h1 id='balance'>$0.00</h1>
+        
+    </>
+  )
+}
